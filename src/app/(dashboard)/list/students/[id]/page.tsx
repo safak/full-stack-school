d55a235769e6e57bb3e86013897611842a17eb9a
Safak <1486366+safak@users.noreply.github.com>
2024-09-21 14:13:21 +0200

    Student shortcuts fixed

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -161,31 +161,31 @@ const SingleStudentPage = async ({
           <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
             <Link
               className="p-3 rounded-md bg-lamaSkyLight"
-              href={`/list/lessons?classId=${2}`}
+              href={`/list/lessons?classId=${student.class.id}`}
             >
               Student&apos;s Lessons
             </Link>
             <Link
               className="p-3 rounded-md bg-lamaPurpleLight"
-              href={`/list/teachers?classId=${2}`}
+              href={`/list/teachers?classId=${student.class.id}`}
             >
               Student&apos;s Teachers
             </Link>
             <Link
               className="p-3 rounded-md bg-pink-50"
-              href={`/list/exams?classId=${2}`}
+              href={`/list/exams?classId=${student.class.id}`}
             >
               Student&apos;s Exams
             </Link>
             <Link
               className="p-3 rounded-md bg-lamaSkyLight"
-              href={`/list/assignments?classId=${2}`}
+              href={`/list/assignments?classId=${student.class.id}`}
             >
               Student&apos;s Assignments
             </Link>
             <Link
               className="p-3 rounded-md bg-lamaYellowLight"
-              href={`/list/results?studentId=${"student2"}`}
+              href={`/list/results?studentId=${student.id}`}
             >
               Student&apos;s Results
             </Link>
