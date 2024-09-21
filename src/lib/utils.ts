f4e6a7d98fd592b895b25ea15f3d9fe72c736bcc
Safak <1486366+safak@users.noreply.github.com>
2024-09-21 14:10:44 +0200

    Weekend bug fixed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,35 +1,29 @@
-const currentWorkWeek = () => {
+// IT APPEARS THAT BIG CALENDAR SHOWS THE LAST WEEK WHEN THE CURRENT DAY IS A WEEKEND.
+// FOR THIS REASON WE'LL GET THE LAST WEEK AS THE REFERENCE WEEK.
+// IN THE TUTORIAL WE'RE TAKING THE NEXT WEEK AS THE REFERENCE WEEK.
+
+const getLatestMonday = (): Date => {
   const today = new Date();
   const dayOfWeek = today.getDay();
-
-  const startOfWeek = new Date(today);
-
-  if (dayOfWeek === 0) {
-    startOfWeek.setDate(today.getDate() + 1);
-  }
-  if (dayOfWeek === 6) {
-    startOfWeek.setDate(today.getDate() + 2);
-  } else {
-    startOfWeek.setDate(today.getDate() - (dayOfWeek - 1));
-  }
-  startOfWeek.setHours(0, 0, 0, 0);
-
-  return startOfWeek;
+  const daysSinceMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+  const latestMonday = today;
+  latestMonday.setDate(today.getDate() - daysSinceMonday);
+  return latestMonday;
 };
 
 export const adjustScheduleToCurrentWeek = (
   lessons: { title: string; start: Date; end: Date }[]
 ): { title: string; start: Date; end: Date }[] => {
-  const startOfWeek = currentWorkWeek();
+  const latestMonday = getLatestMonday();
 
   return lessons.map((lesson) => {
     const lessonDayOfWeek = lesson.start.getDay();
 
     const daysFromMonday = lessonDayOfWeek === 0 ? 6 : lessonDayOfWeek - 1;
 
-    const adjustedStartDate = new Date(startOfWeek);
+    const adjustedStartDate = new Date(latestMonday);
 
-    adjustedStartDate.setDate(startOfWeek.getDate() + daysFromMonday);
+    adjustedStartDate.setDate(latestMonday.getDate() + daysFromMonday);
     adjustedStartDate.setHours(
       lesson.start.getHours(),
       lesson.start.getMinutes(),
