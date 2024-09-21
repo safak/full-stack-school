6b6292f3f704db767bef88ce4322ab69f6caf3e2
Safak <1486366+safak@users.noreply.github.com>
2024-09-21 14:15:07 +0200

    Skeleton loading added

--- src/app/(dashboard)/list/loading.tsx
+++ src/app/(dashboard)/list/loading.tsx
@@ -1,7 +1,29 @@
 const Loading = () => {
   return (
-    <div className='w-full h-full flex items-center justify-center'>Loading...</div>
-  )
-}
+    <div className="p-8 animate-pulse">
+      <div className=" rounded-lg overflow-hidden shadow-md">
+        <div className="p-8 bg-gray-200 flex space-x-32">
+          <div className="h-6 bg-gray-300 rounded w-1/6"></div>
+          <div className="h-6 bg-gray-300 rounded w-2/6"></div>
+          <div className="h-6 bg-gray-300 rounded w-1/6"></div>
+          <div className="h-6 bg-gray-300 rounded w-1/6"></div>
+        </div>
+        <div className="p-4">
+          {[...Array(10)].map((_, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-between mb-4 py-2 mt-4"
+            >
+              <div className="h-8 bg-gray-200 rounded w-1/6 mr-2"></div>
+              <div className="h-8 bg-gray-200 rounded w-2/6 mr-2"></div>
+              <div className="h-8 bg-gray-200 rounded w-1/6 mr-2"></div>
+              <div className="h-8 bg-gray-200 rounded w-1/6"></div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
